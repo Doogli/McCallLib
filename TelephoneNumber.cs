db38179304031dc7fa258f1efea26f8893f40442
Doogli <doug.taylor@mweb.co.za>
2014-05-14 15:40:36 +0200

    fixed protobuf refs
    simplified telnos - add country code parsing later

--- TelephoneNumber.cs
+++ TelephoneNumber.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using ProtoBuf;
 
 namespace McCallLib
@@ -9,25 +6,25 @@ namespace McCallLib
     [ProtoContract]
     public class TelephoneNumber
     {
-        [ProtoMember(1)]
-        public string CountryCode { get; set; }
-        [ProtoMember(2)]
-        public string NetworkCode { get; set; }
-        [ProtoMember(3)]
-        public string UserNumber { get; set; }
+		[ProtoMember(1)]
+		public string Number { get; set; }
+//        [ProtoMember(1)]
+//        public string CountryCode { get; set; }
+//        [ProtoMember(2)]
+//        public string NetworkCode { get; set; }
+//        [ProtoMember(3)]
+//        public string UserNumber { get; set; }
 
-        public TelephoneNumber(string country, string network, string user)
-        {
-            CountryCode = country;
-            NetworkCode = network;
-            UserNumber = user;
-        }
+//        public TelephoneNumber(string country, string network, string user)
+//        {
+//            CountryCode = country;
+//            NetworkCode = network;
+//            UserNumber = user;
+//        }
 
-        public string FullNumber
-        {
-            get{
-                return "+" + CountryCode + NetworkCode + UserNumber;
-            }
-        }
+		public TelephoneNumber(string number)
+		{
+			Number = number;
+		}
     }
 }
