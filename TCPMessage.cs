27e28e6d2a8158017fc49e22d55219234822f71f
Doogli <doug.taylor@mweb.co.za>
2014-05-14 02:32:28 +0200

    in itial commit

---
+++ TCPMessage.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using ProtoBuf;
+using System.Net;
+
+namespace McCallLib
+{
+    public enum MessageCodes
+    {
+        OK = 0,
+        AltServer,
+        Subscribe,
+        Unsubscribe
+    }
+
+    [ProtoContract]
+    [ProtoInclude(2, typeof(AlternateServerMessage))]
+    [ProtoInclude(3, typeof(SubscribeMessage))]
+    [ProtoInclude(4, typeof(UnsubscribeMessage))]
+    public class TCPMessage
+    {
+        [ProtoMember(1)]
+        public MessageCodes MessageCode { get; set; }
+
+        public TCPMessage(MessageCodes code)
+        {
+            MessageCode = code;
+        }
+    }
+
+    [ProtoContract]
+    public class AlternateServerMessage : TCPMessage
+    {
+        [ProtoMember(2)]
+        public IPAddress IpAddress { get; set; }
+
+        public AlternateServerMessage(IPAddress ip)
+            : base(MessageCodes.AltServer)
+        {
+            IpAddress = ip;
+        }
+    }
+
+    [ProtoContract]
+    public class SubscribeMessage : TCPMessage
+    {
+        [ProtoMember(3)]
+        public TelephoneNumber TelNo { get; set; }
+
+        public SubscribeMessage(TelephoneNumber telNo)
+            : base(MessageCodes.Subscribe)
+        {
+            TelNo = telNo;
+        }
+    }
+
+    [ProtoContract]
+    public class UnsubscribeMessage : TCPMessage
+    {
+        [ProtoMember(4)]
+        public TelephoneNumber TelNo { get; set; }
+
+        public UnsubscribeMessage(TelephoneNumber telNo)
+            : base(MessageCodes.Unsubscribe)
+        {
+            TelNo = telNo;
+        }
+    }
+}
---
+++ TelephoneNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProtoBuf;
+
+namespace McCallLib
+{
+    [ProtoContract]
+    public class TelephoneNumber
+    {
+        [ProtoMember(1)]
+        public string CountryCode { get; set; }
+        [ProtoMember(2)]
+        public string NetworkCode { get; set; }
+        [ProtoMember(3)]
+        public string UserNumber { get; set; }
+
+        public TelephoneNumber(string country, string network, string user)
+        {
+            CountryCode = country;
+            NetworkCode = network;
+            UserNumber = user;
+        }
+
+        public string FullNumber
+        {
+            get{
+                return "+" + CountryCode + NetworkCode + UserNumber;
+            }
+        }
+    }
+}
