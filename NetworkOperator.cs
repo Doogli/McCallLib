bcee6c7d75632cc4792ce66efde078ba3b28dafc
Doogli <doug.taylor@mweb.co.za>
2014-05-20 05:22:54 +0200

    added NetworkOperator class and message

---
+++ NetworkOperator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ProtoBuf;
+
+namespace McCallLib
+{
+    [ProtoContract]
+    public class NetworkOperator
+    {
+        [ProtoMember(1, IsRequired = true)]
+        public string CountryISO { get; set; }
+
+        [ProtoMember(2, IsRequired = true)]
+        public string OperatorCode { get; set; }
+
+        [ProtoMember(3, IsRequired = true)]
+        public string OperatorName { get; set; }
+
+        public NetworkOperator()
+        {
+        }
+
+        public NetworkOperator(string countryISO, string operatorCode, string operatorName)
+        {
+            CountryISO = countryISO;
+            OperatorCode = operatorCode;
+            OperatorName = operatorName;
+        }
+    }
+}
+
